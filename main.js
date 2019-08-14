e2587407932dd84064d49eba42c82a7ff836518a
Wilian da Silva Oliveira <wiliandasilvaoliveira1994@gmail.com>
2019-08-14 05:35:27 -0300

    finalizando o curso ES6 ROCKESEAT

--- main.js
+++ main.js
@@ -19,8 +19,32 @@ class App {
             description: "http://wilian.com.br",
             avatar_url: "www.con.br"
         })
+        this.render()
+    }
 
-        console.log(this.repositories)
+    render(){
+        this.listEl.innerHtml = ''
+
+        this.repositories.forEach(repo => {
+            let imgEl = document.createElement('img')
+            imgEl.setAttribute('src', repo.avatar_url);
+
+            let titleEl = document.createElement('strong')
+            titleEl.appendChild(document.createTextNode(repo.name))
+
+            let descriptionEl = document.createElement('p')
+            descriptionEl.appendChild(document.createTextNode(repo.description))
+
+            let linkEl = document.createElement('a');
+            linkEl.setAttribute('target', '_blank');
+            linkEl.appendChild(document.createTextNode('Acessar'))
+
+            let listItemEl = document.createElement('li')
+            listItemEl.appendChild(imgEl)
+            listItemEl.appendChild(titleEl)
+            listItemEl.appendChild(descriptionEl)
+            listItemEl.appendChild(linkEl)
+        })
     }
 }
 
